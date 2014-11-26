3f0d4384e3fbbf2427f10736c3e9e1e24296c049
sami purmonen <purmonen@kth.se>
2014-11-26 23:10:06 +0100

    kopt addition

--- TSP/main.cpp
+++ TSP/main.cpp
@@ -13,29 +13,42 @@
 #include <math.h>
 #include <map>
 #include <assert.h>
+#include <chrono>
+#include <list>
 
 using namespace std;
-
+auto startTime = chrono::high_resolution_clock::now();
 
 class TravelingSalesmanProblem{
 public:
     int points;
     double *x;
     double *y;
+    double *distance;
     
     static TravelingSalesmanProblem *createFromStdin(){
         TravelingSalesmanProblem *instance = new TravelingSalesmanProblem();;
         cin >> instance->points;
+        instance->distance = new double[instance->points * instance->points];
         instance->y = new double[instance->points];
         instance->x = new double[instance->points];
         for (auto i = 0; i < instance->points; i++)
             cin >> instance->x[i] >> instance->y[i];
+        
+        for (int i = 0; i < instance->points; i++) {
+            for (int j = 0; j < instance->points; j++) {
+                instance->distance[i*instance->points+j] = (instance->x[i]-instance->x[j])*(instance->x[i]-instance->x[j]) + (instance->y[i]-instance->y[j])*(instance->y[i]-instance->y[j]);
+                
+            }
+        }
         return instance;
     }
     
     static TravelingSalesmanProblem *testInstance(){
         TravelingSalesmanProblem *instance = new TravelingSalesmanProblem();;
         instance->points = 10;
+        
+        
         instance->x = new double[instance->points];
         instance->y = new double[instance->points];
         instance->x[0] = 95.0129;		//	95.0129 61.5432
@@ -58,11 +71,20 @@ public:
         instance->y[8] = 41.0270;
         instance->x[9] = 44.4703;		//	44.4703 89.3650
         instance->y[9] = 89.3650;
+        
+        
+        instance->distance = new double[instance->points * instance->points];
+        for (int i = 0; i < instance->points; i++) {
+            for (int j = 0; j < instance->points; j++) {
+                instance->distance[i*instance->points+j] = sqrt((instance->x[i]-instance->x[j])*(instance->x[i]-instance->x[j]) + (instance->y[i]-instance->y[j])*(instance->y[i]-instance->y[j]));
+                
+            }
+        }
         return instance;
     }
     
     double dist(int a, int b){
-        return sqrt((x[a] - x[b])*(x[a] - x[b]) + (y[a] - y[b])*(y[a] - y[b]));
+        return distance[a * points+b];
     }
     
     double best = 9999999;
@@ -166,7 +188,7 @@ public:
                                 //cout << "tmpS = " << tmpS << endl;
                                 //cout << "m = " << m << endl;
                                 //cout << "k = " << k << endl;
-//                                assert(C[tmpS][m] != 0);
+                                //                                assert(C[tmpS][m] != 0);
                                 //8 4 1
                                 //1101 1
                                 //1111
@@ -205,14 +227,14 @@ public:
         
         double opt = 9999999;
         //        printBits((1 << ()n) - 1);
-        int node = -1;
+        int node = 0;
         unsigned long S = (1 << n) - 1;
         for (int k = 1; k < n; k++) {
             double tmp = C[S][k];
             opt = min(tmp, opt);
             node = k;
         }
-
+        
         while (true) {
             cout << node << endl;
             if (node == 0) {
@@ -246,20 +268,123 @@ public:
         }
         for (int i = 0; i < points; i++) {
             vector.push_back(tour[i]);
+            //            cout << tour[i] << endl;
         }
         return vector;
+    }
+    
+    double tourDistance(vector<int> tour) {
+        double distance = 0;
+        for (int i = 1; i < tour.size(); i++) {
+            distance += dist(tour[i-1], tour[i]);
+        }
+        return distance + dist(tour[0], tour[tour.size()-1]);
+    }
+    
+    vector<int> kopt(vector<int> tour) {
+        int timeout = 1500;
+        
+        list<int> a(10);
+        
+        while (startTime + chrono::milliseconds(timeout) > chrono::high_resolution_clock::now()) {
+            bool didSwap = false;
+            for (int i = 0; i < tour.size(); i++) {
+                if (startTime + chrono::milliseconds(timeout) < chrono::high_resolution_clock::now()) {
+                    break;
+                }
+                for (int j = 0; j < tour.size(); j++) {
+                    if (i != j) {
+                        int prevI = i-1 >= 0 ? i-1 : tour.size()-1;
+                        int prevJ = j-1 >= 0 ? j-1 : tour.size()-1;
+                        int nextI = i+1 < tour.size() ? i+1 : 0;
+                        int nextJ = j+1 < tour.size() ? j+1 : 0;
+                        double prevDistanceI = dist(tour[prevI], tour[i]) + dist(tour[i], tour[nextI]);
+                        double prevDistanceJ = dist(tour[prevJ], tour[j]) + dist(tour[j], tour[nextJ]);
+                        swap(tour[j], tour[i]);
+                        double afterDistanceI = dist(tour[prevI], tour[i]) + dist(tour[i], tour[nextI]);
+                        double afterDistanceJ = dist(tour[prevJ], tour[j]) + dist(tour[j], tour[nextJ]);
+                        if (afterDistanceI + afterDistanceJ < prevDistanceI + prevDistanceJ) {
+                            //                            cout << "Swapping" << endl;
+                            didSwap = true;
+                        } else {
+                            swap(tour[j], tour[i]);
+                        }
+                    }
+                }
+            }
+            if (!didSwap) {
+                break;
+            }
+        }
+        return tour;
+    }
+    
+    
+    vector<int> kopt3(vector<int> tour) {
+        int timeout = 1500;
+        
+        list<int> a(10);
+        
+        while (startTime + chrono::milliseconds(timeout) > chrono::high_resolution_clock::now()) {
+            bool didSwap = false;
+            for (int i = 0; i < tour.size(); i++) {
+                if (startTime + chrono::milliseconds(timeout) < chrono::high_resolution_clock::now()) {
+                    break;
+                }
+                for (int j = 0; j < tour.size(); j++) {
+                    
+                    for (int k = 0; k < tour.size(); k++) {
+                        
+                        if (i != j && j != k && i != k) {
+                            int prevI = i-1 >= 0 ? i-1 : tour.size()-1;
+                            int prevJ = j-1 >= 0 ? j-1 : tour.size()-1;
+                            int prevK = k-1 >= 0 ? k-1 : tour.size()-1;
+                            int nextI = i+1 < tour.size() ? i+1 : 0;
+                            int nextJ = j+1 < tour.size() ? j+1 : 0;
+                            int nextK = k+1 < tour.size() ? k+1 : 0;
+                            
+                            double prevDistanceI = dist(tour[prevI], tour[i]) + dist(tour[i], tour[nextI]);
+                            double prevDistanceJ = dist(tour[prevJ], tour[j]) + dist(tour[j], tour[nextJ]);
+                            double prevDistanceK = dist(tour[prevK], tour[k]) + dist(tour[k], tour[nextK]);
+                            swap(tour[j], tour[i]);
+                            swap(tour[j], tour[k]);
+                            double afterDistanceI = dist(tour[prevI], tour[i]) + dist(tour[i], tour[nextI]);
+                            double afterDistanceJ = dist(tour[prevJ], tour[j]) + dist(tour[j], tour[nextJ]);
+                            double afterDistanceK = dist(tour[prevK], tour[k]) + dist(tour[k], tour[nextK]);
+                            if (afterDistanceI + afterDistanceJ + afterDistanceK < prevDistanceI + prevDistanceJ+ prevDistanceK) {
+                                //                            cout << "Swapping" << endl;
+                                didSwap = true;
+                            } else {
+                                swap(tour[j], tour[i]);
+                                swap(tour[j], tour[k]);
+                            }
+                        }
+                    }
+                }
+            }
+            if (!didSwap) {
+                break;
+            }
+        }
+        return tour;
     }
 };
 
 
 int main(int argc, char **argv){
-    auto instance = TravelingSalesmanProblem::createFromStdin();
-    instance->dynamicExact();
-    
-    //    for (auto node: instance->greedy()) {
-    //        cout << node << endl;
+    auto instance = TravelingSalesmanProblem::testInstance();
+    //    if (instance->points <= 19) {
+    //        instance->dynamicExact();
+    //    } else {
+    //        instance->greedy();
     //    }
-    //    cout << instance->best << endl;
+    
+    auto tour = instance->kopt(instance->greedy());
+    for (auto node: tour) {
+        cout << node << endl;
+    }
+    //            cout << instance->tourDistance(instance->greerdy()) << endl;
+                cout << instance->tourDistance(tour) << endl;
     
     //    instance->solvenaiveExactly(10, 0, 0);
     //    cout << instance->best << endl;
